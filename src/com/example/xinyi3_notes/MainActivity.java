917938b63de3d17f4262ab10bec05389484a47fd
xinyi3 <wxy604@hotmail.com>
2014-09-26 02:52:06 -0600

    Update MainActivity.java

--- src/com/example/xinyi3_notes/MainActivity.java
+++ src/com/example/xinyi3_notes/MainActivity.java
@@ -1,21 +1,3 @@
-/*    This file is part of xinyi3_notes.
-	
-	Copyright (C) 2014 Xin Yi Wang xinyi3@ualberta.ca
-	
-    xinyi3-notes is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    xinyi3-notes is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with xinyi3_notes.  If not, see <http://www.gnu.org/licenses/>.
-    
-    */
 
 package com.example.xinyi3_notes;
 
